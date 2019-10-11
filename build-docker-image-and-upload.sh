73f755ed6c3619a8f8adf468c71e079cba01e47d
Nilton Constantino <niltonrc@gmail.com>
2019-10-11 12:05:38 +0100

    fix my docker hub repository address

--- build-docker-image-and-upload.sh
+++ build-docker-image-and-upload.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-docker build --build-arg JAR_FILE=launcher-app/build/libs/launcher-app-1.0-SNAPSHOT.jar -t simple-forwarding/simple-forwarding:1.0 -f simple-forwarding.dockerFile .
+docker build --build-arg JAR_FILE=launcher-app/build/libs/launcher-app-1.0-SNAPSHOT.jar -t bquarkz/simple-forwarding:1.0 -f simple-forwarding.dockerFile .
 docker login -u="bquarkz"
-docker push simple-forwarding/simple-forwarding:1.0
+docker push bquarkz/simple-forwarding:1.0
 docker logout
